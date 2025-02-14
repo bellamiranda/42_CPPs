2fd9db34a2710342ef9d78409d698d4011edd07b
Isabella Miranda Camargo De Lima <ismirand@c1r8s4.42porto.com>
2025-02-14 14:54:57 +0000

    ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ismirand <ismirand@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/14 14:04:02 by ismirand          #+#    #+#             */
+/*   Updated: 2025/02/14 14:17:09 by ismirand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; argv[i]; i++)
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char)toupper(argv[i][j]);
+		std::cout << std::endl;
+	}
+	return (0);
+}
